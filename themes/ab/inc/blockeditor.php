6cb2c4a0bb017a44c0c7f5c0a81d79cd142b8f86
Layma <layma.companioni@dcmail.ca>
2022-02-17 23:58:47 -0500

    Enqueued js

---
+++ themes/ab/inc/blockeditor.php
@@ -0,0 +1,15 @@
+<?php 
+/**
+ * Functions which enhance the theme by hooking into WordPress
+ *
+ * @package Aroma_Boutique
+ */
+function myguten_enqueue() {
+    wp_enqueue_script(
+        'myguten-script',
+        plugins_url( 'myguten.js', __FILE__ ),
+        array( 'wp-blocks', 'wp-dom-ready', 'wp-edit-post' ),
+        filemtime( plugin_dir_path( __FILE__ ) . '/myguten.js' )
+    );
+}
+add_action( 'enqueue_block_editor_assets', 'myguten_enqueue' );
